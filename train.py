36cd5d57d9ee2af6bc046d6d795920cc66580da5
陈百见 <602141901@qq.com>
2020-01-20 22:44:31 +0800

    完善

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import os
+import matplotlib.pyplot as plt
 from input_data import MyDataset
 from models.simple import MySimpleNet
 
@@ -17,9 +19,6 @@ def CAL_TOP5_ACC(logits, labels):
 	概率最高的前五个字符，如果包含了labels中指代的字符，即视为正确。
 	'''
 
-	#labels = tf.argmax(labels, 1)
-	#batch_size = labels.shape[0]
-
 	sorted_logits_index = tf.argsort(logits, direction = "DESCENDING")
 	threshold = tf.cast(tf.ones_like(labels)*5, tf.int32)#因为是top5，所以最大的5五个数值的序号为0、1、2、3、4，所以定为5
 	top5 = tf.cast(tf.less(sorted_logits_index, threshold), tf.float32)#top5对应的数值为1，其他为0
@@ -31,26 +30,40 @@ def CAL_TOP5_ACC(logits, labels):
 if __name__ == "__main__":
 
 	#设置训练时的常数
-	BATCH_SIZE = 32
+	TRAIN_BATCH_SIZE = 32
+	TEST_BATCH_SIZE	= 128
 	CLASS = 100
 	IMG_H, IMG_W = 256, 256
 	LEARNING_RATE = 10E-3
-	TRAIN_STEPS = 1000
+	TRAIN_STEPS = 100
+	#多少步打印一次训练情况
+	CHECK_STEP = 20
+	#多少步保存一次数据
+	SAVE_STEP = 200
 
+	#准备数据
 	train_dir = "D:\\BaiduNetdiskDownload\\TMD\\train\\train"
-	dataset = MyDataset(train_dir)
+	test_dir = "D:\\BaiduNetdiskDownload\\TMD\\test1"
+	test_csv = "D:\\BaiduNetdiskDownload\\TMD\\label-test1-fake.csv"
+	dataset = MyDataset(train_dir, test_dir, test_csv)
 
 	train_dataset = tf.data.Dataset.from_generator(dataset.train_datagen, output_types = (tf.float32, tf.float32))
-	train_dataset = train_dataset.shuffle(20).batch(BATCH_SIZE).repeat()
+	train_dataset = train_dataset.shuffle(20).batch(TRAIN_BATCH_SIZE).repeat()
 	train_iterator = train_dataset.make_one_shot_iterator()
-
 	next_train_data, next_train_labels = train_iterator.get_next()
+
+	test_dataset = tf.data.Dataset.from_generator(dataset.test_datagen, output_types = (tf.float32, tf.float32))
+	test_dataset = test_dataset.shuffle(20).batch(TEST_BATCH_SIZE).repeat()
+	test_iterator = test_dataset.make_one_shot_iterator()
+	next_test_data, next_test_labels = test_iterator.get_next()
 
 	X = tf.placeholder(tf.float32, [None, IMG_H, IMG_W, 1], name = "X")
 	Y = tf.placeholder(tf.float32, [None, CLASS], name = "Y")
 
 	net = MySimpleNet(CLASS)
+	#前向传播
 	outputs = net(X)
+	#获取这个运算
 	tf.add_to_collection("outputs", outputs)
 
 	loss_op = tf.losses.softmax_cross_entropy(logits = outputs, onehot_labels = Y)
@@ -60,20 +73,54 @@ if __name__ == "__main__":
 
 	history = {}
 	history["train_loss"] = []
-	history["train_acc"] = []
+	history["train_top1_acc"] = []
+	history["train_top5_acc"] = [] 
+	history["test_loss"] = []
+	history["test_top1_acc"] = []
+	history["test_top5_acc"] = []
 
 	config = tf.ConfigProto(log_device_placement = False, allow_soft_placement = True)
+	#开始训练
 	with tf.Session(config = config) as sess:		
 
+		saver = tf.train.Saver(max_to_keep = 10)
+
+		#初始化
 		sess.run(tf.global_variables_initializer())
 
 		for step in range(1, TRAIN_STEPS + 1):
 
+			#获取训练集中的batch数据
 			train_data, train_labels = sess.run((next_train_data, next_train_labels))
-			#train_labels = tf.one_hot(train_labels, depth = CLASS)
-			#train_loss, top1_acc, top5_acc, _ = sess.run((loss_op, top1_acc_op, top5_acc_op, train_op), feed_dict = {X:train_data, Y:train_labels})
-			_, train_loss, top1_acc, top5_acc= sess.run((train_op, loss_op, top1_acc_op, top5_acc_op), feed_dict = {X:train_data, Y:train_labels})
-			#print("Step %s, train loss %.4f, top1 acc %.2f%, top5 acc %.2f%"%(step, train_loss, top1_acc*100, top5_acc*100))
-			if step % 50 == 0:
-				print("Step %s, train loss %.2f, top1 acc %.2f%%, top5 acc %.2f%%."%(step, train_loss, top1_acc*100, top5_acc*100))
+			if step % CHECK_STEP == 0:
 
+				#获取测试集中的batch数据
+				test_data, test_labels = sess.run((next_test_data, next_test_labels))
+				#使用训练集数据更新模型，同时计算对应的损失和准确率
+				_, train_loss, top1_acc, top5_acc = sess.run((train_op, loss_op, top1_acc_op, top5_acc_op), feed_dict = {X:train_data, Y:train_labels})
+				#计算在测试集上的损失数值和top5准确率
+				test_loss, test_top5_acc = sess.run((loss_op, top5_acc_op), feed_dict = {X:test_data, Y:test_labels})
+				string_tuple = (step, train_loss, top1_acc*100, top5_acc*100, test_loss, test_top5_acc*100)
+				print("Step %s, train loss %.2f, top1 acc %.2f%%, top5 acc %.2f%%, test loss %.2f, test top5 acc %.2f%%."%string_tuple)
+			else:
+				#只训练，不计算，提高效率
+				sess.run(train_op, feed_dict = {X:train_data, Y:train_labels})
+
+			#如果适时需要保存模型
+			#if step % CHECK_STEP == 0:
+			#	saver.save(sess, os.path.join("D:\\GitFile\\calligraphy\\models\\TrainResult", "simple"), global_step = step)
+
+		num_check = range(1, len(history["train_loss"]) + 1)
+		plt.subplot(1, 3, 1)
+		plt.plot(num_check, history["train_loss"], "r", label = "train loss")
+		plt.plot(num_check, history["test_loss"], 'b', label = "test loss")
+
+		plt.subplot(1, 3, 2)
+		plt.plot(num_check, history["train_top1_acc"], "r", label = "train top1 acc")
+
+		plt.subplot(1, 3, 3)
+		plt.plot(num_check, history["train_top5_acc"], "r", label = "train top5 acc")
+		plt.plot(num_check, history["test_top5_acc"], "b", label = "test top5 acc")
+
+		plt.legend()
+		plt.show()
